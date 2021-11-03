b1d6be40873ab8f30bc1eb6498d27575077acdda
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-11-04 01:13:50 +0600

    Route for api

--- api_app/routes/api.php
+++ api_app/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\firstApi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("data",[firstApi::class,'getData']);
